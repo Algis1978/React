66f176e8498e3244fc9d8f3703b3f738157a771f
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-10-18 08:24:48 +0300

    min

--- my-ap/src/App.jsx
+++ my-ap/src/App.jsx
@@ -1,15 +1,11 @@
 import Skritulys from "./components/Skritulys"
 import skrituliuSkaicius from "./functions/skrituliuGeneratorius"
-import Banner from "./components/Banner"
-
 const skrituliai = skrituliuSkaicius(10)
 
 //Funkcija App
 function App() {
   return (
     <>
-      <Banner/>
-
       {skrituliai.map( (skritulys, index) => <Skritulys key={index} circleColor={skritulys.circleColor} circleBorder={skritulys.circleBorder} circleNumber={skritulys.circleNumber}/>)}
     </>
   );
