bbb95ba35932af5401634e44e1dc476b603e9621
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-10-20 22:15:46 +0300

    min

--- my-ap/src/App.jsx
+++ my-ap/src/App.jsx
@@ -5,6 +5,7 @@ import FieldAnimal from "./components/FieldAnimal";
 function App() {
 
     const [field, setField] = useState([])
+    const [selectField, setSelectField] = useState([])
 
     const add = (what) => {
         const fieldCopy = field.slice();
@@ -13,21 +14,38 @@ function App() {
         console.log(fieldCopy);
     }
 
+    const choose = (what) => {
+        const selectFieldCopy = selectField.slice();
+        selectFieldCopy.push({laukas:what});
+        setSelectField(selectFieldCopy);
+        console.log(selectFieldCopy);
+    }
+
     // useEffect(() => {
     //     console.log('susirenderino')
     // }, []);
 
     return (
+        <>
         <div className="field">
+            <div className="laukoNr">Pirmas laukas</div>
         <div>
           {field.map((fieldAnimal, i)=><FieldAnimal key={i} fieldAnimal={fieldAnimal}></FieldAnimal>)}
         </div>
+        </div>
+        <div className="field">
+        <div className="laukoNr">Antras laukas</div>
+        <div>
+          {field.map((fieldAnimal, i)=><FieldAnimal key={i} fieldAnimal={fieldAnimal}></FieldAnimal>)}
+        </div>
+        </div>
+        <button className = "mygtukas3" onClick={() => choose('laukas1')}>Pirmas laukas</button>
+        <button className = "mygtukas3" onClick={() => choose('laukas2')}>Antras laukas</button>
         <button className = "mygtukas4"  onClick={() => add('cow')}>Pridėti karvę</button>
         <button className = "mygtukas4" onClick={() => add('sheep')}>Pridėti avį</button>
         <button className = "mygtukas4" onClick={() => add('chicken')}>Pridėti vištą</button>
-        </div>
+        </>
     );
-
 }
 
 export default App;
