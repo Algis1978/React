b52554e0e8dde1435a4b71d15924ee61940d705f
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-10-26 13:28:21 +0300

    min

---
+++ my-ap/server/App.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = 3003
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+app.use(cors());
+
+app.use(express.urlencoded({
+    extended: true
+    }))
+app.use(express.json());
+
+//Routingas - nustatomas kelias narsykles uzklausai
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World! Yeahhhh! Fuck you</h1>')
+})
+//req - is stringo padarytas objektas, ateinantis is narsykles
+// res - atsakymas narsyklei
+// labas dvitaskis nurodo parametra (id)
+app.get('/labas', (req, res) => {
+  res.send(`<h1>UHU-HUB-UH-U<br> ${req.params.id}</h1>`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
