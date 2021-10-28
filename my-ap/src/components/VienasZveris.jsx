a24ba2c494fccc6f2f4a0072035231e0b5688fab
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-10-28 10:21:16 +0300

    min

--- my-ap/src/components/VienasZveris.jsx
+++ my-ap/src/components/VienasZveris.jsx
@@ -4,7 +4,13 @@ export default function VienasZveris ( {vzveris}) {
     return (
         <>
         <div className="vienasZveris">
-            <div>{vzveris["Vardas"]}</div>
+            <div className="vardas">{vzveris["Vardas"]}</div>
+            <div className="invnr">Inv. Nr.: {vzveris["Inv. Nr."]}</div>
+            <div className="rusis"><span className="text">Rūšis:</span> {vzveris["Rūšis"]}</div>
+            <div className="lytis"><span className="text">Lytis:</span> {vzveris["Lytis"]}</div>
+            <div className="atvykimodata"><span className="text">Registracijos data:</span> {vzveris["Atvykimo data"].slice(0, 10)}</div>
+            <div className="mase"><span className="text">Masė:</span> {vzveris["Masė"]} kg</div>
+            <div className="islaikymas"><span className="text">Išlaikymas per mėn.:</span> {vzveris["Išlaikymo kaina eurais per mėn."]} eur</div>
         </div>
         </>
     )
