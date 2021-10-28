6148b83720d8200dc305f06ac0378e80ee70f94c
Algis1978 <86305896+Algis1978@users.noreply.github.com>
2021-10-28 20:08:49 +0300

    min

--- my-ap/src/components/VienasZveris.jsx
+++ my-ap/src/components/VienasZveris.jsx
@@ -5,10 +5,10 @@ export default function VienasZveris ( {vzveris}) {
         <>
         <div className="vienasZveris">
             <div className="vardas">{vzveris["Vardas"]}</div>
-            <div className="invnr">Inv. Nr.: {vzveris["Inv. Nr."]}</div>
+            <div className="invnr">Reg. nr.: {vzveris["Reg. nr."]}</div>
+            <div className="atvykimodata"><span className="text">Registracijos data:</span> {vzveris["Registracijos data"].slice(0, 10)}</div>
             <div className="rusis"><span className="text">Rūšis:</span> {vzveris["Rūšis"]}</div>
             <div className="lytis"><span className="text">Lytis:</span> {vzveris["Lytis"]}</div>
-            <div className="atvykimodata"><span className="text">Registracijos data:</span> {vzveris["Atvykimo data"].slice(0, 10)}</div>
             <div className="mase"><span className="text">Masė:</span> {vzveris["Masė"]} kg</div>
             <div className="islaikymas"><span className="text">Išlaikymas per mėn.:</span> {vzveris["Išlaikymo kaina eurais per mėn."]} eur</div>
         </div>
--- my-ap/src/components/ZverisSukurti.jsx
+++ my-ap/src/components/ZverisSukurti.jsx
@@ -20,10 +20,19 @@ export default function ZverisSukurti ( {sukurti} ) {
             <h2>Įveskite naują žvėrį:</h2>
             <div className="menu">
             <p><span className="text">Vardas:</span></p><input type="text" value={inputs["Vardas"]} onChange={(e) => formControl(e, "Vardas")} require />
+            <p><span className="text">Registracijos data:</span></p><input type="text" value={inputs["Registracijos data"]} onChange={(e) => formControl(e, "Registracijos data")} require />
+
             <p><span className="text">Rūšis:</span></p><input type="text" value={inputs["Rūšis"]} onChange={(e) => formControl(e, "Vardas")} require />
-            <p><span className="text">Lytis:</span></p><input type="text" value={inputs["Lytis"]} onChange={(e) => formControl(e, "Vardas")} require />
-            <p><span className="text">Registracijos data:</span></p><input type="date" value={inputs["Registracijos data"]} onChange={(e) => formControl(e, "Registracijos data")} require />
+
+            <p><span className="text"><label for="lytis">Lytis (pasirinkite):</label></span></p>
+
+                <select name="lytis" id="lytis">
+                <option value="patinas">patinas</option>
+                <option value="patelė">patelė</option>
+                </select>
+
             <p><span className="text">Masė (kg):</span></p><input type="number" value={inputs["Masė"]} onChange={(e) => formControl(e, "Masė")} require />
+
             <p><span className="text">Išlaikymas per mėn. (eur):</span></p><input type="number" value={inputs["Išlaikymas per mėn."]} onChange={(e) => formControl(e, "Išlaikymas per mėn.")} require />
             </div>
             <button onClick={handleSukurti}>Patvirtinkite</button>
